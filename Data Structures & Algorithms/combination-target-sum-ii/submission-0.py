c224307bc1d342834723263d13f9354cbc3d0686
brendes6 <143545666+brendes6@users.noreply.github.com>
2026-08-27 13:55:55 -0400

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # same as 1 but dont consider current index as candidate?
+
+        # [1, 2, 2, 4, 5, 6, 9]
+
+
+        results = []
+
+        stack = []
+
+        def backtrack(nums, i):
+            if sum(stack)==target:
+                results.append(stack.copy())
+                return
+            if sum(stack) > target:
+                return
+            if i >= len(nums):
+                return
+            
+
+            for j in range(i, len(nums)):
+                # Some condition here to prevent recursing on duplicate elements,
+                # BUT we still want to use them together. e.g. using 2 and 2 in the SAME
+                # stack, but not making seperate stacks like [2], [2] or [1,2], [1, 2]
+
+                if j > i and nums[j] == nums[j-1]:
+                    continue
+                stack.append(nums[j])
+                backtrack(nums, j+1)
+                stack.pop()
+        
+        backtrack(sorted(candidates), 0)
+        return results
